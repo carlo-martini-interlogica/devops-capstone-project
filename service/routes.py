e9cbeb55f30e6057005e910c646bdeb1bdf49a34
carlo-martini-interlogica <115990960+carlo-martini-interlogica@users.noreply.github.com>
2023-07-18 11:53:06 +0200

    Update routes.py
    Linting

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -72,10 +73,10 @@ def list_accounts():
         app.logger.info("Returning [%s] accounts", len(account_list))
         return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
         """
@@ -87,6 +88,7 @@ def get_accounts(account_id):
         if not account:
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
         return account.serialize(), status.HTTP_200_OK
+
 
 #####################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -104,6 +106,7 @@ def update_accounts(account_id):
         account.deserialize(request.get_json())
         account.update()
         return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
@@ -124,7 +127,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
